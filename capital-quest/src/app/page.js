cc224a15c1efa999cc1bd0f80a85332201639fe8
PaulaFregene <paulafregene@gmail.com>
2024-08-01 10:35:51 -0500

    logos
    logos

--- capital-quest/src/app/page.js
+++ capital-quest/src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <main className=" grad flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <div>Introducing</div>
+        <Image src="/capital quest outlined logo.png" width={500} height={500} alt="Capital Quest logo image"/>
         <div>LEARN FINANCIAL LITERACY WITH CAPITAL ONE! </div>
       </div>
     </main>
