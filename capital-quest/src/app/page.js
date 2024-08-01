6b0d3a4867b81fb779a4595d8d6f8d0ec2a1124c
Hector Baeza <baezah@oregonstate.edu>
2024-08-01 12:56:27 -0700

    Final Spalsh screen

--- capital-quest/src/app/page.js
+++ capital-quest/src/app/page.js
@@ -1,20 +1,20 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=" grad flex min-h-screen flex-col items-center justify-between p-24">
-      <div className= 'text-center' >
-        <div>Introducing</div>
-        <Image src="/capital quest outlined logo.png" width={500} height={500} alt="Capital Quest logo image"/>
-        <div>LEARN FINANCIAL LITERACY WITH CAPITAL ONE! </div>
+    <main className="grad flex min-h-screen flex-col items-center justify-between pt-[-20px] p-0">
+      <div className="flex flex-col items-center justify-center flex-grow text-center p-10 space-y-[-40px] mb-[200px]">
+        <Typography variant="h4" style={{color:'white', fontWeight: 'bold'}}>Introducing</Typography>
+        <Image src="/capital quest outlined logo.png" width={500} height={500} alt="Capital Quest logo image" />
+        <Typography variant="h6" style={{ color: 'white', fontWeight: 'bold', fontSize: '28px' }}>LEARN FINANCIAL LITERACY WITH CAPITAL ONE!</Typography>  
       </div>
-      <div>
-        <Button variant="contained" color="error" size="large" href="#contained-buttons"> Begin</Button>
+      <div className="text-center p-0 mb-[200px]">
+        <Button variant="contained" color="error" size="large" href="#contained-buttons" sx = {{fontSize:'1.5rem', padding:'20px 94px', borderRadius: '40px' }}> Begin</Button>
       </div>
-      
+      <div className="flex justify-center mt-auto">
       <Image src="/capital one logo.png" width={200} height={200} alt="Capital One logo image"/>
-      
+      </div>   
     </main>
   );
 }
