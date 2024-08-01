db4b1a6b02c55ac38cc16ea26053d822443ab6ca
PaulaFregene <paulafregene@gmail.com>
2024-08-01 10:06:24 -0500

    capital one logo
    capital one logo

--- capital-quest/src/app/page.js
+++ capital-quest/src/app/page.js
@@ -2,9 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className=" grad flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        Hello, World
+        <div>Introducing</div>
+        <div>LEARN FINANCIAL LITERACY WITH CAPITAL ONE! </div>
       </div>
     </main>
   );
