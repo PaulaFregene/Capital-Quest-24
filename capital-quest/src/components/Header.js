dc3b5c89541523728454d1523480a3bdccb00eb4
Hector Baeza <baezah@oregonstate.edu>
2024-08-01 23:55:05 -0700

    Improved saving + credit pages

--- capital-quest/src/components/Header.js
+++ capital-quest/src/components/Header.js
@@ -4,13 +4,13 @@ import Link from "next/link"
 export default function Header() {
     return (
         <div>
-            <div className= "flex flex-row items-start justify-between">
+            <div className= "flex flex-start flex-row p-10">
                 <button>
                     <Link href="/game_home" passHref>
-                        <Image src="/Menu.png" width={200} height={200} alt="Menu button" />
+                        <Image src="/Menu.png" width={50} height={50} alt="Menu button" />
                     </Link>
                 </button>
-                <Image src="/capital quest outlined logo.png" width={300} height={300} alt="Capital One logo image"/>
+                <Image src="/capital quest outlined logo.png" width={300} height={300} alt="Capital One logo image" className="ml-auto mr-10"/>
             </div>
         </div>
     )
