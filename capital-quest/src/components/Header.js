a3312103b95443663e5576dd93b7cf052606e0fe
PaulaFregene <paulafregene@gmail.com>
2024-08-01 19:59:54 -0500

    Savings Page

--- capital-quest/src/components/Header.js
+++ capital-quest/src/components/Header.js
@@ -3,13 +3,15 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <div className= "flex min-h-screen flex-row items-start justify-between p-0">
-            <button>
-                <Link href="/" passHref>
-                    <Image src="/Menu.png" width={200} height={200} alt="Menu button" />
-                </Link>
-            </button>
-            <Image src="/capital quest outlined logo.png" width={300} height={300} alt="Capital One logo image"/>
+        <div>
+            <div className= "flex flex-row items-start justify-between">
+                <button>
+                    <Link href="/" passHref>
+                        <Image src="/Menu.png" width={200} height={200} alt="Menu button" />
+                    </Link>
+                </button>
+                <Image src="/capital quest outlined logo.png" width={300} height={300} alt="Capital One logo image"/>
+            </div>
         </div>
     )
 }
